ee264fb4fd6731f2d468d542cdbe96f7b4e07267
Alexander Grube <alexp.grube@googlemail.com>
2023-05-21 17:42:09 +0200

    day 1 and 2

--- src/bin/aoc2.rs
+++ src/bin/aoc2.rs
@@ -17,7 +17,7 @@ fn aoc2() {
     println!("Games: {:?}", games);
     // calculate the score for each game
     let mut scores: Vec<i32> = Vec::new();
-    for game in games {
+    for game in &games {
         let mut score = 0;
         // get the player moves
         let moves: Vec<&str> = game.split(" ").collect();
@@ -60,6 +60,55 @@ fn aoc2() {
     println!("Scores: {:?}", scores);
     // calculate the total score
     let mut total_score = 0;
+    for score in scores {
+        total_score += score;
+    }
+    println!("Total score: {}", total_score);
+
+    scores = Vec::new();
+    for game in games {
+        let mut score = 0;
+        // get the player move and outcome
+        let moves: Vec<&str> = game.split(" ").collect();
+        println!("Moves: {:?}", moves);
+        let player1_move = moves[0];
+        let game_outcome = moves[1];
+        // calculate the score
+        match game_outcome {
+            "X" => {
+                score += 0;
+                match player1_move {
+                    "A" => score += 3,
+                    "B" => score += 1,
+                    "C" => score += 2,
+                    _ => println!("Invalid move: {}", player1_move),
+                }
+            }
+            "Y" => {
+                score += 3;
+                match player1_move {
+                    "A" => score += 1,
+                    "B" => score += 2,
+                    "C" => score += 3,
+                    _ => println!("Invalid move: {}", player1_move),
+                }
+            }
+            "Z" => {
+                score += 6;
+                match player1_move {
+                    "A" => score += 2,
+                    "B" => score += 3,
+                    "C" => score += 1,
+                    _ => println!("Invalid move: {}", player1_move),
+                }
+            }
+            _ => println!("Invalid move: {}", game_outcome),
+        }
+        scores.push(score);
+    }
+    println!("Scores: {:?}", scores);
+    // calculate the total score
+    total_score = 0;
     for score in scores {
         total_score += score;
     }
